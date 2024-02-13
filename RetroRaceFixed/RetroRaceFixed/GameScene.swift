56f87eddc72add2dd0d00a2c0d0a9da9d859a815
czenge25 <czenge25@kentdenver.org>
2024-02-13 11:22:20 -0700

    current commit caught up

--- RetroRaceFixed/RetroRaceFixed/GameScene.swift
+++ RetroRaceFixed/RetroRaceFixed/GameScene.swift
@@ -1,109 +1,354 @@
-//
-//  GameScene.swift
-//  RetroRaceFixed
-//
-//  Created by Cameron Zenge on 2/13/24.
-//
+/*
+CZ, HK
+12/21/23
+ */
 
 import SpriteKit
 import GameplayKit
 
 class GameScene: SKScene {
     
-    var entities = [GKEntity]()
-    var graphs = [String : GKGraph]()
+    // Player and joystick vars
+    var player : SKNode?
+    var joystick : SKNode?
+    var joystickKnob : SKNode?
+    var xArrow : SKNode?
+    var yArrow : SKNode?
     
-    private var lastUpdateTime : TimeInterval = 0
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+    // Brake vars
+    var brakeButton: SKSpriteNode?
+    var isBraking = false
+    var isTouchingRoad = true
     
-    override func sceneDidLoad() {
+    var maxSpeed: CGFloat = 600.0
+    var grassMaxSpeed: CGFloat?
+    var currentMaxSpeed: CGFloat?
+    
+    var acceleration: CGFloat = 4
+    var grassAcceleration: CGFloat?
+    var currentAcceleration: CGFloat?
+    
+    var friction: CGFloat = 0.995
+    
+    // MapBuilder vars
+    var sceneName : String?
+    var mp : MapBuilder?
+    var roadTileArray: [SKSpriteNode] = []
 
-        self.lastUpdateTime = 0
+    // Camera var
+    var sceneCamera : SKCameraNode = SKCameraNode()
+    
+    // Joystick variables
+    var joystickAction = false
+    var xKnobRadius: CGFloat = 50.0
+    var yKnobRadius: CGFloat = 50.0
+    
+    // Sprite Engine
+    var previousTimeInterval : TimeInterval = 0
+    var playerIsFacingRight = true
+    var playerSpeedX: CGFloat = 1.0
+    var playerSpeedY: CGFloat = 1.0
+    
+    // Pi constant
+    let pi = CGFloat.pi
+
+    // didMove
+    override func didMove(to view: SKView) {
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
+        grassMaxSpeed = maxSpeed * (3/5)
+        grassAcceleration = acceleration * (0.3/0.7)
         
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
+        player = childNode(withName: "Car1")
         
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
+        joystick = childNode(withName: "joystick")
+        joystickKnob = joystick?.childNode(withName: "knob")
+        xArrow = joystick?.childNode(withName: "xArrow")
+        yArrow = joystick?.childNode(withName: "yArrow")
+        
+        joystick?.position = CGPoint(x: -300,y: -100)
+        joystickKnob?.position = CGPoint(x: 0, y: 0)
+        xArrow?.position = CGPoint(x: 0, y: 0)
+        yArrow?.position = CGPoint(x: 0, y: 0)
+        joystick?.zPosition = 3
+        
+        player?.position = CGPoint(x: 0,y: 0)
+        player?.zPosition = 2
+        
+        
+        // Create brake button as SKSpriteNode
+        let brakeButtonTexture = SKTexture(imageNamed: "BrakeButton")
+        brakeButton = SKSpriteNode(texture: brakeButtonTexture)
+        brakeButton?.scale(to: CGSize(width: 100, height: 300))
+        brakeButton?.name = "brakeButton"
+        brakeButton?.zPosition = 3
+        brakeButton?.position = CGPoint(x: 300, y: -175)
+        
+        addChild(brakeButton!)
+
+        camera = sceneCamera
+        
+        // Set up physics for the player
+        player?.physicsBody = SKPhysicsBody(rectangleOf: player!.frame.size)
+        
+        self.physicsWorld.gravity = CGVector(dx: 0, dy: 0)
+        
+        player?.position = CGPoint(x: 0, y: 0)
+        
+// MARK: Map Selection
+        mp = MapBuilder(scene: "Tutorial")
+        sceneName = mp?.scene
+        
+        if (sceneName == "Tutorial") {
+            // Creating road tiles
+            var count = 0...5
+            for i in count {
+                let roadTile = SKSpriteNode(imageNamed: "Road_01_Tile_03")
+                roadTileArray.append(roadTile)
+                roadTile.yScale = 0.25
+                roadTile.xScale = 1.75
+                roadTile.zPosition = 1
+                roadTile.position.x = CGFloat(896 * i)
+                roadTile.position.y = 0
+                addChild(roadTile)
+            }
             
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
+            // Creating grass tiles
+            count = 0...15
+            var numbers = 0...25
+            for i in numbers {
+                for j in count {
+                    let grassTile = SKSpriteNode(imageNamed: "Grass_Tile")
+                    grassTile.yScale = 0.5
+                    grassTile.xScale = 0.5
+                    grassTile.zPosition = 0
+                    grassTile.position.x = CGFloat(256 * i) - 1280
+                    grassTile.position.y = CGFloat(256 * j) - 2560
+                    addChild(grassTile)
+                }
+            }
+            
+        } else if (sceneName == "Level1") {
+            // Code for Level1
+        } else if (sceneName == "Level2") {
+            // Code for Level2
+        } else {
+            // Code for other scenes
         }
-    }
-    
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
-    }
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
-        
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
-    }
-    
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    
-    override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
-        
-        // Initialize _lastUpdateTime if it has not already been
-        if (self.lastUpdateTime == 0) {
-            self.lastUpdateTime = currentTime
-        }
-        
-        // Calculate time since last update
-        let dt = currentTime - self.lastUpdateTime
-        
-        // Update entities
-        for entity in self.entities {
-            entity.update(deltaTime: dt)
-        }
-        
-        self.lastUpdateTime = currentTime
     }
 }
+
+// MARK: Touches
+extension GameScene {
+    // Touch Began
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for touch in touches {
+            // Check if the touch is on the brakeButton
+            if let brakeButton = brakeButton {
+                let location = touch.location(in: self)
+                isBraking = brakeButton.frame.contains(location)
+            }
+            // Check if the touch is on the joystickKnob
+            if let joystickKnob = joystickKnob {
+                let location = touch.location(in: joystick!)
+                joystickAction = joystickKnob.frame.contains(location)
+            }
+        }
+    }
+    
+    // Touch Moved
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let joystick = joystick else { return }
+        guard let joystickKnob = joystickKnob else { return }
+        
+        if !joystickAction { return }
+        
+        // Move the joystick knob based on touch distance
+        for touch in touches {
+            let position = touch.location(in: joystick)
+            let xLength = position.x
+            let yLength = position.y
+
+            let length = sqrt(pow(xLength, 2) + pow(yLength, 2))
+
+            if length < xKnobRadius {
+                joystickKnob.position = position
+            } else {
+                let angle = atan2(yLength, xLength)
+                let xMovement = cos(angle) * xKnobRadius
+                let yMovement = sin(angle) * yKnobRadius
+                joystickKnob.position = CGPoint(x: xMovement, y: yMovement)
+            }
+        }
+    }
+    
+    // Touch End
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        isBraking = false
+        for touch in touches {
+            // Check joystick coordinates and reset knob position if necessary
+            let xJoystickCoordinate = touch.location(in: joystick!).x
+            let xLimit: CGFloat = 200.0
+            if xJoystickCoordinate > -xLimit && xJoystickCoordinate < xLimit {
+                resetKnobPosition()
+            }
+            let yJoystickCoordinate = touch.location(in: joystick!).y
+            let yLimit: CGFloat = 200.0
+            if yJoystickCoordinate > -yLimit && yJoystickCoordinate < yLimit {
+                resetKnobPosition()
+            }
+        }
+    }
+}
+
+extension GameScene {
+    // Reset joystick knob position
+    func resetKnobPosition() {
+        let initialPoint = CGPoint(x: 0, y: 0)
+        let moveBack = SKAction.move(to: initialPoint, duration: 0.1)
+        moveBack.timingMode = .linear
+        joystickKnob?.run(moveBack)
+        joystickAction = false
+    }
+}
+
+// MARK: Game Loop
+extension GameScene {
+    // Game loop for player movement and actions
+    override func update(_ currentTime: TimeInterval) {
+        // Update camera position based on player position
+        camera?.position.x = player?.position.x ?? 0
+        camera?.position.y = player?.position.y ?? 0
+        
+        // Update joystick position based on camera position
+        joystick?.position.x = (camera?.position.x ?? 0) - 300
+        joystick?.position.y = (camera?.position.y ?? 0) - 100
+        
+        let deltaTime = currentTime - previousTimeInterval
+        previousTimeInterval = currentTime
+        
+        // Player Movement
+        guard let joystickKnob = joystickKnob else { return }
+        let xPosition = Double(joystickKnob.position.x)
+        let yPosition = Double(joystickKnob.position.y)
+        
+        brakeButton?.position.x = (player?.position.x ?? 0) + 300
+        brakeButton?.position.y = (player?.position.y ?? 0) - 175
+        
+        for roadTileArray in self.roadTileArray {
+            if player?.frame.intersects(roadTileArray.frame) ?? false {
+                // Change the variable based on the collision
+                isTouchingRoad = true
+                break;
+            } else {
+                isTouchingRoad = false
+            }
+        }
+                
+        if isTouchingRoad {
+                friction = 0.7
+                currentAcceleration = acceleration
+            currentMaxSpeed = maxSpeed
+            } else {
+                friction = 0.3
+                currentMaxSpeed = grassMaxSpeed
+                currentAcceleration = grassAcceleration
+            }
+        
+        if isBraking {
+            // Apply braking force
+            let brakeFriction: CGFloat = 0.98
+            
+            player?.physicsBody?.velocity.dx *= brakeFriction
+            player?.physicsBody?.velocity.dy *= brakeFriction
+            
+            if (abs(player?.physicsBody?.velocity.dx ?? 0) < 5.0 && abs(player?.physicsBody?.velocity.dy ?? 0) < 5.0) {
+                player?.physicsBody?.velocity = CGVector(dx: 0, dy: 0)
+                isBraking = false
+            }
+        }
+        
+        if (joystickAction) {
+            // Calculate force components and apply force to the player's physics body
+            let xForce = CGFloat(xPosition) * playerSpeedX * deltaTime
+            let yForce = CGFloat(yPosition) * playerSpeedY * deltaTime
+            
+            /*
+            
+            player?.physicsBody?.applyForce(CGVector(dx: xForce, dy: yForce))
+            
+            player?.physicsBody?.velocity.dx = max(min((player?.physicsBody?.velocity.dx ?? 0) + xForce * acceleration, maxSpeed), -maxSpeed)
+            player?.physicsBody?.velocity.dy = max(min((player?.physicsBody?.velocity.dy ?? 0) + yForce * acceleration, maxSpeed), -maxSpeed)
+             
+             */
+            
+            let angle = atan2(yForce, xForce)
+            
+            // Apply the force in the direction of the joystick
+            let impulse = CGVector(dx: xForce, dy: yForce)
+            player?.physicsBody?.applyImpulse(impulse)
+            
+            // Limit the player's speed
+            let speed = sqrt(pow(player?.physicsBody?.velocity.dx ?? 0, 2) + pow(player?.physicsBody?.velocity.dy ?? 0, 2))
+            if speed > currentMaxSpeed ?? 0 {
+                player?.physicsBody?.velocity.dx *= (currentMaxSpeed ?? 0) / speed
+                player?.physicsBody?.velocity.dy *= (currentMaxSpeed ?? 0) / speed
+            }
+            
+            player?.zRotation = angle
+        } else {
+            // Apply friction to gradually reduce velocity when there's no input
+            player?.physicsBody?.velocity.dx *= friction
+            player?.physicsBody?.velocity.dy *= friction
+            
+            let velocity = physicsBody?.velocity
+
+            player?.physicsBody?.velocity.dx = 0.0
+            player?.physicsBody?.velocity.dy = 0.0
+        }
+        
+        if let playerPhysicsBody = player?.physicsBody {
+            let speed = abs(hypot(playerPhysicsBody.velocity.dx, playerPhysicsBody.velocity.dy))
+            if speed > currentMaxSpeed ?? 0 {
+                playerPhysicsBody.velocity.dx *= ((currentMaxSpeed ?? 0)/speed)
+                playerPhysicsBody.velocity.dy *= ((currentMaxSpeed ?? 0)/speed)
+            }
+        }
+        
+        print("Player Position: \(player?.position ?? .zero)")
+        print("Player Is Hidden: \(player?.isHidden ?? true)")
+        print("Joystick Action: \(joystickAction)")
+        print(isTouchingRoad)
+    }
+}
+
+// MARK: Collisions
+
+// *Not currently properly functional*
+
+/*
+extension GameScene: SKPhysicsContactDelegate{
+    
+    struct Collision{
+        enum Masks: Int{
+            case killing, player, reward, ground
+            var bitmask: UInt32{ return 1<<self.rawValue}
+        }
+        let masks: (first: UInt32, second:UInt32)
+        func matches (first: Masks, second: Masks) -> Bool{
+            return (first.bitmask==masks.first && second.bitmask == masks.second) ||
+            (first.bitmask==masks.second && second.bitmask == masks.first)
+        }
+    }
+    func didBegin(_ contact: SKPhysicsContact) {
+        let collision = Collision(masks: (first: contact.bodyA.categoryBitMask, second: contact.bodyB.categoryBitMask))
+        
+        if collision.matches(first: .player, second: .killing) {
+            let die = SKAction.move(to: CGPoint(x: -300, y: -100), duration: 0.0)
+            player?.run(die)
+        }
+    }
+    
+}
+*/
+
+
